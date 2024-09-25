a447eb8171b466d6f8d2d84b206dbf19eed67d46
sonu singh <ssonusingh123abc@gmail.com>
2024-09-25 22:14:51 +0530

    Add files via upload

---
+++ square_pattern.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i, j, n = 4;
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= n; j++)
+        {
+            cout << j;
+        }
+        cout << endl;
+    }
+    return 0;
+}
